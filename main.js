95c3c391cbac6da2bd5fe074083cc867a07e8978
crisps85 <crisps2@ymail.com>
2015-09-01 20:12:28 +0100

    Hover to reveal quick view and heart logo at top right corner
    trying to scale image and hide overflow instead of putting the image as
    a background in CSS.

--- main.js
+++ main.js
@@ -1,14 +1,56 @@
 $(document).ready(function(){
 
-	//when i click on the menu icon
-	//show the nav
-	//when i click on the menu icon again
-	//hide the nav
 	$('.first').on('mouseenter', function(){
 		$('.first a').slideToggle({direction:"up"});
 		$(".first a").show();
 	});
 
+	$('.first').on('mouseleave', function(){
+		$('.first a').slideToggle({direction:"down"});
+		$(".first a").hide();
+	});
+
+	$('.first').on('mouseenter', function(){
+		$('.first .heart img').slideToggle({direction:"up"});
+		$(".first .heart img").show();
+	});
+
+	$('.first').on('mouseleave', function(){
+		$('.first .heart img').slideToggle({direction:"down"});
+		$(".first .heart img").hide();
+	});
+
+	$('.second').on('mouseenter', function(){
+		$('.second a').slideToggle({direction:"up"});
+		$(".second a").show();
+		$(".second .heart img").show();
+	});
+
+	$('.second').on('mouseleave', function(){
+		$('.second a').slideToggle({direction:"down"});
+		$(".second a").hide();
+	});
+
+	$('.second').on('mouseleave', function(){
+		$('.second .heart img').slideToggle({direction:"down"});
+		$(".second .heart img").hide();
+	});
+
+	$('.third').on('mouseenter', function(){
+		$('.third a').slideToggle({direction:"up"});
+		$(".third a").show();
+		$(".third .heart img").show();
+	});
+
+	$('.third').on('mouseleave', function(){
+		$('.third a').slideToggle({direction:"down"});
+		$(".third a").hide();
+	});
+
+	$('.third').on('mouseleave', function(){
+		$('.third .heart img').slideToggle({direction:"down"});
+		$(".third .heart img").hide();
+	});
 /*	$('.first').on('mouseleave'),function(){
 		$('.first a').slideToggle({direction:"up"});
 		$('.first a').hide();
@@ -31,4 +73,5 @@ $(document).ready(function(){
 		}
 
 	});*/
+
 });
